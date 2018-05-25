4a415cae1f758f0a2b791d8f64d0afc9e818df1e
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-25 23:19:54 +0300

    laravel passport set up

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(StoreUserRequest $request)
+    {
+      $user = new User;
+      $user->username = $request->username;
+      $user->email = $request->email;
+      $user->password = bcrypt($request->password);
+
+      $user->save();
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Laravel\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
