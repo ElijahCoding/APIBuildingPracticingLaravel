3c103346463c08c0752d2cd1f4d1180ef06cade4
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-26 20:48:25 +0300

    creating topics

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Topic;
+use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTopicRequest;
 
@@ -9,6 +11,15 @@ class TopicController extends Controller
 {
     public function store(StoreTopicRequest $request)
     {
+      $topic = new Topic;
+      $topic->title = $request->title;
+      $topic->user()->associate($request->user());
 
+      $post = new Post;
+      $post->body = $request->body;
+      $post->user()->associate($request->user());
+
+      $topic->save();
+      $topic->posts()->save($post);
     }
 }
